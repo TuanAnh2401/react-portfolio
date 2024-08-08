4e8a602de98a470de1524210c1798516337756e4
TuanAnh2401 <lta24012k2@gmail.com>
2024-08-08 11:43:51 +0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,97 @@
+const logotext = "ANH";
+const meta = {
+    title: "Tuan Anh",
+    description: "I’m Tuan Anh Full stack devloper,currently working in Ho Chi Minh",
+};
+
+const introdata = {
+    title: "I’m Tuan Anh",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop apps",
+    },
+    description: "As a senior student Information Technology student at Hutech University, I have extensive programming and information technology experience.",
+    your_img_url: "https://camo.githubusercontent.com/0eda36005abd9bf7e72584afc2f6ef1e808a357cb65a07fc2fe5036ba5268df7/68747470733a2f2f692e70696e696d672e636f6d2f6f726967696e616c732f65382f66342f35332f65386634353334363961336563393765636433353464663436356437333931332e676966",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "I am a passionate software developer with years of experience in the field. I strive to create innovative and efficient solutions. With a strong foundation in programming languages and problem-solving skills, I am confident in contributing to the success of any project. Programming is not just a job for me; it's a passion and a commitment. Besides my professional interests, I enjoy traveling, reading stories, listening to music, and playing soccer.",
+};
+
+const worktimeline = [{
+        jobtitle: "Back End Developer",
+        where: "HD Bank",
+        date: "2024",
+    },
+    {
+        jobtitle: "Full Stack Developer",
+        where: "Hutech Information Technology Management Center",
+        date: "2019",
+    }
+];
+
+const skills = [
+    {
+        name: ".NET",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 80,
+    }
+];
+
+const services = [
+    {
+        title: "Backend Development",
+        description: "Developing robust and scalable backend systems using various technologies like Node.js, .NET, and Java.",
+    },
+    {
+        title: "Frontend Development",
+        description: "Creating dynamic and responsive front-end applications using React, Vue.js, and other frameworks.",
+    },
+    {
+        title: "Database Management",
+        description: "Designing, implementing, and managing databases using MySQL, MongoDB, PostgreSQL, and more.",
+    },
+    {
+        title: "UI & UX Design",
+        description: "Creating intuitive and visually appealing interfaces that enhance user experience.",
+    },
+    {
+        title: "Mobile App Development",
+        description: "Building responsive and efficient mobile applications for Android and iOS platforms.",
+    },
+    {
+        title: "WordPress Development",
+        description: "Designing and developing custom WordPress themes and plugins to meet specific business needs.",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "lta2401.it@gmail.com",
+    YOUR_FONE: "0327807422",
+    description: "Feel free to reach out with any inquiries or feedback. I am here to assist you with all your needs. Whether you have a question, a comment, or just want to say hello, I'm excited to hear from you. Let's connect and make something great together!",
+    YOUR_SERVICE_ID: "service_myj51me",
+    YOUR_TEMPLATE_ID: "template_imq605i",
+    YOUR_USER_ID: "oxSPAVTjY9R03jgLV",
+};
+
+const socialprofils = {
+    github: "https://github.com/TuanAnh2401",
+    facebook: "https://www.facebook.com/letuananh2401",
+    linkedin: "https://www.linkedin.com/in/lta2401/"
+};
+export {
+    meta,
+    dataabout,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/header/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+import { logotext } from "../content_option";
+import Themetoggle from "../components/themetoggle";
+
+const Headermain = () => {
+  const [isActive, setActive] = useState("false");
+
+  const handleToggle = () => {
+    setActive(!isActive);
+    document.body.classList.toggle("ovhidden");
+  };
+
+  return (
+    <>
+      <header className="fixed-top site__header">
+        <div className="d-flex align-items-center justify-content-between">
+          <Link  className="navbar-brand nav_ac" to="/">
+            {logotext}
+          </Link>
+          <div className="d-flex align-items-center">
+          <Themetoggle />
+          </div>
+        </div>
+      </header>
+      <div className="br-top"></div>
+      <div className="br-bottom"></div>
+      <div className="br-left"></div>
+      <div className="br-right"></div>
+      
+    </>
+  );
+};
+
+export default Headermain;
