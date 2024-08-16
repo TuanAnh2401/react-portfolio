bc7c33021913f28ebad50dfadba3e511d42628f4
TuanAnh2401 <lta24012k2@gmail.com>
2024-08-16 11:14:23 +0700

    Up

--- src/content_option.js
+++ src/content_option.js
@@ -7,10 +7,10 @@ const meta = {
 const introdata = {
     title: "I’m Tuan Anh",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop apps",
-    },
+        first: "Passionate about coding",
+        second: "Crafting amazing websites",
+        third: "Building innovative apps",
+    },    
     description: "As a senior student Information Technology student at Hutech University, I have extensive programming and information technology experience.",
     your_img_url: "https://camo.githubusercontent.com/0eda36005abd9bf7e72584afc2f6ef1e808a357cb65a07fc2fe5036ba5268df7/68747470733a2f2f692e70696e696d672e636f6d2f6f726967696e616c732f65382f66342f35332f65386634353334363961336563393765636433353464663436356437333931332e676966",
 };
